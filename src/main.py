786dad59e90a03064a6d4bbf1fd534ca816c5516
Fredrik Stål <fredrik.stal@ingka.ikea.com>
2021-08-19 14:45:21 +0200

    Fix create version

--- src/main.py
+++ src/main.py
@@ -69,7 +69,7 @@ def _close_issues(version_id):
       data=payload
     )
 
-def _update_version(version_id, release_version):
+def _release_and_update_version(version_id, release_version):
   jira_host = os.environ.get(HOSTNAME)
 
   url = f"https://{jira_host}/rest/api/2/version/{version_id}"
@@ -85,13 +85,14 @@ def _update_version(version_id, release_version):
     "released": True
   })
 
-  return requests.put(url, 
-                      headers=headers,
-                      data=payload
-                      )
+  requests.put(url, 
+                  headers=headers,
+                  data=payload
+                  )
+  return release_version
 
 def _release_version(version_id):
-  _update_version(version_id, os.environ.get(VERSION))
+  return _release_and_update_version(version_id, os.environ.get(VERSION))
 
 def _create_new_version(version_id):
   jira_host = os.environ.get(HOSTNAME)
@@ -116,11 +117,11 @@ def main(request):
   version_id = _get_version_id()
 
   if version_id:
-    if os.environ.get(RELEASE_VERSION):
-      _update_version(version_id, os.environ.get(RELEASE_VERSION))
-    else:
-      _release_version(version_id)
     _close_issues(version_id)
+    if os.environ.get(RELEASE_VERSION):
+      version_id = _release_and_update_version(version_id, os.environ.get(RELEASE_VERSION))
+    else:
+      version_id = _release_version(version_id)
     _create_new_version(version_id)
   else:
     logging.info("No version updated or released")
